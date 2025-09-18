665ae0df7e3f2bac5bd0e12ea45c6be6d3134828
fengkuangshenye-coder <fengkuangshenye@gmail.com>
2025-09-19 08:34:52 +1400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue } from "clsx";
-import { clsx } from "clsx";
+// src/lib/utils.ts
+import type { ClassValue } from "clsx";
+import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
