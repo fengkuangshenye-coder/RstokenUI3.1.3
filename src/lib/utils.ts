7b1254f21a13683618ea7a9cba1a96d5949c1b03
fengkuangshenye-coder <fengkuangshenye@gmail.com>
2025-09-19 10:42:55 +1400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+// src/lib/utils.ts
+export function cn(...classes: Array<string | undefined | false | null>) {
+  return classes.filter(Boolean).join(" ");
 }
