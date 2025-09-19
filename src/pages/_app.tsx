471f998803825f81bc38eb70a3d1e06a09829034
fengkuangshenye-coder <fengkuangshenye@gmail.com>
2025-09-19 14:02:12 +1400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 // src/pages/_app.tsx
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import type { AppProps } from 'next/app';
+// 正确路径：从 src/pages 到 src/styles
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
